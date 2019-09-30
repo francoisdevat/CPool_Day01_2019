d096638e29be7025e1c6a53dd2997fa7bef55d61
françois de vathaire <fvathaire@localhost.localdomain>
2019-09-30 09:12:00 -0400

    add_of_prepare_my-repo.sh

---
+++ prepare_my_repo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+read repositorie
+blih -u francois.de-vathaire@epitech.eu repository create $repositorie
+blih -u francois.de-vathaire@epitech.et repository setacl $repositorie ramassage-tek r
